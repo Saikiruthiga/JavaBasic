eb0a9da76b7911de6e2bf484bf05e34617821e97
Vinoth <Vinoth@Vinoth.home>
2024-01-19 11:34:38 +0100

    MethodsBasics

---
+++ src/org/jav/Methods.java
@@ -0,0 +1,32 @@
+package org.jav;
+
+public class Methods {
+	
+	public static void main(String args[]) {
+		
+		Methods m = new Methods();
+		m.getData();
+		String s = m.getData1();
+		System.out.println(s);
+		
+		// we can get the return type and then print or directly we can call inside of print statement.
+		
+		System.out.println(m.getData1());
+		
+		getdata2();
+		
+	}
+	
+	public void getData() {
+		System.out.println("Hello World");  // if there is return statement in the block we should have the data type in the declaration.
+	}
+	public String getData1() {
+		return "Welcome!!!";
+	}
+	
+	public static void getdata2() {
+		System.out.println("Selenium");
+	}
+
+}
+
